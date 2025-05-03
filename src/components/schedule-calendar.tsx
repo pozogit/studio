17c61f30199144e6389941a70bc97071eef596c1
Cristian Pozo <cristian.pozoe@gmail.com>
2025-05-03 05:14:06 +0000

    Enla vista de al dar semana siguiente no esta pasando a la siguiente, si retrocede pero no avanza

--- src/components/schedule-calendar.tsx
+++ src/components/schedule-calendar.tsx
@@ -1,4 +1,5 @@
 
+
 "use client"
 
 import * as React from "react"
@@ -619,4 +620,5 @@ export function ScheduleCalendar({ allShifts, setShifts }: ScheduleCalendarProps
       </TooltipProvider>
   )
 }
+
 
